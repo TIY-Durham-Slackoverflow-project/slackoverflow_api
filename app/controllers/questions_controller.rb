4f653e8ecec40657284818dd61bc6cba125ba8c5
christopherwperry <christopherwperry@gmail.com>
2017-09-27 13:17:10 -0400

    initial commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,35 @@
+class QuestionsController < ApplicationController
+  
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def create
+    @question = Question.create(question_params)
+    render :show, status: :created
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update!(question_params)
+    render :show
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    render json: {deleted: true}
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body, :author)
+  end
+
+end
