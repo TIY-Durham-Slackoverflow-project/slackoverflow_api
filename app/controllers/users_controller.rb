02690284a3090abbaf56ed4e804cfb57c63e3802
christopherwperry <christopherwperry@gmail.com>
2017-09-27 15:50:19 -0400

    Add method to User controller for creating a new user and logging a user in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render status: :created
+    else
+      render json: {
+        errors: @user.errors
+      }, status: :bad_request
+    end
+  end
+
+  def login
+    user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+
+    if !user
+      render status: :unauthorized, json: {
+        "error": "There is no user with that email and password"
+      }
+    else
+      render json: {token: user.token}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :avatar, :bio_text, :questions_asked_num, :questions_answered_num)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_secure_token
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
 end
