47ac318e2eee9b2933ce6d212d523da30ee5d32f
christopherwperry <christopherwperry@gmail.com>
2017-09-29 15:55:59 -0400

    Add dependent destroy action for all answers that belong to a question.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
-  has_many :votes
+  has_many :votes, dependent: :destroy
 
   validates_presence_of :body
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,8 @@
 class Question < ApplicationRecord
   serialize :tags, Array
   belongs_to :user
-  has_many :answers
-  has_many :votes
+  has_many :answers, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   validates_presence_of :title, :body
 end
